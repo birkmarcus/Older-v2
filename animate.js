83aacce88bafa17a6c4604383b8527853ea78c20
Birk Marcus <birkmarcus@gmail.com>
2014-08-22 17:02:49 +0200

    Knap til kryds JS edit

--- animate.js
+++ animate.js
@@ -37,6 +37,43 @@ $(document).ready(function () {
         }
     });
 
+
+
+
+
+$("#knapTop").click(function () {
+        if($('#knapTop').hasClass('activeKnap')) {
+            $('#knapTop').removeClass('activeKnap');   
+        } else {
+            $('#knapTop').addClass('activeKnap');
+        }
+    });
+
+$("#knapBund").click(function () {
+        if($('#knapBund').hasClass('activeKnap')) {
+            $('#knapBund').removeClass('activeKnap');   
+        } else {
+            $('#knapBund').addClass('activeKnap');
+        }
+    });
+
+$("#knapVenstre").click(function () {
+        if($('#knapVenstre').hasClass('activeKnap')) {
+            $('#knapVenstre').removeClass('activeKnap');   
+        } else {
+            $('#knapVenstre').addClass('activeKnap');
+        }
+    });
+
+$("#knapHojre").click(function () {
+        if($('#knapHojre').hasClass('activeKnap')) {
+            $('#knapHojre').removeClass('activeKnap');   
+        } else {
+            $('#knapHojre').addClass('activeKnap');
+        }
+    });
+
+
     //BXSlider
 $('.bxslider').bxSlider({});
 });
